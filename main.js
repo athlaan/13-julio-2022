e08efbf8a1fc8d3613cd72eb93853fd06e41d249
アトランゼラ <102254054+athlaan@users.noreply.github.com>
2022-07-13 17:37:42 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,29 @@
+let props = {
+  humidity: 'Humedad',
+  wind_speed: 'Velocidad del viento',
+  wing_deg: 'Direccion del viento',
+  description: 'Descripcion',
+  temp: 'Temperatura'
+
+}
+
+let status = {
+  Despejado: '🌞',
+  'Algo nublado': '⛅',
+
+}
+
+
+
+
+document.body.children[1].value = '';
 const url = 'https://ws.smn.gob.ar/map_items/weather';
 const weatherStation = 'Las Rosas';
 
 document.addEventListener('DOMContentLoaded', init());
 
 function init() {
-  document.body.childen[1].value = '';
+  document.body.children[1].value = '';
   //console.log('anda...');
   fetch(url)
     .then(res => res.json())
@@ -14,6 +33,17 @@ function init() {
       //console.log(document.body.children[1].value);
       weatherStation = document.body.children[1].value || weatherStation;
       let filteredData = data.filter(item => item.name == weatherStation)[0];
+      if (!filteredData){
+        document.body.childen[1].value = '';
+        console.log('No existe esa estacion');
+        let alert = document.createElement('h2');
+        alert.textContent = 'No existe esa estacion';
+        document.body.append(alert);
+        setTimeout(() => {
+          alert.remove
+        }, 2000);
+        return false;
+      }
       // console.log(filteredData,weather);
       let stationName = filteredData.name;
       //let {
@@ -34,11 +64,24 @@ function init() {
       delete filteredData.weather.st;
       delete filteredData.weather.tempDesc;
       delete filteredData.weather.visibility;
-      delete filteredData.weather.pressure;
+      delete filteredData.weather.presssure;
+      let dataDiv = document.createElement('div');
+      let itemsDiv = document.createElement('div');
+      dataDiv.classList.add('data');
+      itemsDiv.classList.add('item');
+      container.append(dataDiv);
+      dataDiv.append(itemsDiv)
       for(let key in filteredData.weather) {
+        if (key == 'description') {
+          let emoji = document.createElement('div');
+          emoji.textContent = status[filteredData.weather[key]]
+          emoji.classList.add('weatherStatus');
+          dataDiv.append(emoji);
+        }
         let div = document.createElement('div');
-        div.textContent = `${key}: ${filteredData.weather[key]}`;
-        container.append(div);
+        div.textContent = `${props[key]}: ${filteredData.weather[key]}`;
+        div.classList.add('weatherItem');
+        itemsDiv.append(div);
       }
     })
     .catch(err => console.log(err));
